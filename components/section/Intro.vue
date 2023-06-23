c07fcbed786fb38bcf35082edfc98385281d39e5
rowenslee <rowenslee@TL-ACER.(none)>
2023-06-23 19:47:51 +0700

    Fix contact btn

--- components/section/Intro.vue
+++ components/section/Intro.vue
@@ -22,9 +22,10 @@
       </div>
       <div>
         <NuxtLink to="/contact">
-          <BtnMain>
+          <button
+            class="h-12 font-semibold tracking-wider text-white uppercase rounded-full hover:border-2 w-60 bg-gradient-to-r from-dark-blue to-dark-blue-2/80 border-dark-blue hover:border-white">
             Contact Us
-          </BtnMain>
+          </button>
         </NuxtLink>
       </div>
     </div>
--- pages/contact.vue
+++ pages/contact.vue
@@ -5,3 +5,13 @@
     <NavFooter />
   </div>
 </template>
+
+<script>
+import Vue from 'vue'
+import VueCarousel from '@chenfengyuan/vue-carousel'
+Vue.use(VueCarousel);
+
+export default Vue.extend({
+  name: 'ContactPage'
+})
+</script>
