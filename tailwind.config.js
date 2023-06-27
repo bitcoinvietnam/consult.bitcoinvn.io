021c609173e03f4870dc6b933276d40a8f98a410
rowenslee <rowenslee@TL-ACER.(none)>
2023-06-27 10:02:29 +0700

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,19 @@ module.exports = {
       colors: {
         'dark-blue-2': '#0b1d27',
         'dark-blue': '#374758',
-        'star-yellow': '#FFE26E'
+        'star-yellow': '#FFE26E',
+        gold: {
+          50: '#FEFCE8',
+          100: '#FEF9C3',
+          200: '#FEF08A',
+          300: '#FDE047',
+          400: '#FACC15',
+          500: '#EAB308',
+          600: '#CA8A04',
+          700: '#A16207',
+          800: '#854D0E',
+          900: '#713F12',
+        },
       }
     },
   },
